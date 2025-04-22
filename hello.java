098d19c7317a118e2bce461f77d4e6872ffecb6b
KumudKode <raj94971@gmail.com>
2025-04-22 20:02:06 +0530

    yaya

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
     System.out.println("hello world");
     System.out.println("kairo this side");
     System.out.println("siya this side");
+    System.out.println("yayayayayayay");
 }
 }
